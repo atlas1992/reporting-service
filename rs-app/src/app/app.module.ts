d358c52f2db067f86e52d766778642e3165fd31b
atlas1992 <lxnliangxiao@gmail.com>
2017-09-22 22:59:38 -0400

    Add title, menu and card design

--- rs-app/src/app/app.module.ts
+++ rs-app/src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule}  from '@angular/platform-browser/animations';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MdButtonModule, MdCheckboxModule, MdCardModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +11,12 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    NoopAnimationsModule,
+    MdButtonModule,
+    MdCheckboxModule,
+    MdCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
