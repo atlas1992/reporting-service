d9787263c9c19db423e6e2d30f4f92044071db76
atlas1992 <lxnliangxiao@gmail.com>
2017-09-24 15:21:14 -0400

    Add routes

--- rs-app/src/app/app.component.ts
+++ rs-app/src/app/app.component.ts
@@ -1,9 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  home: boolean;
+  editor: boolean;
+
+  ngOnInit() {
+    this.home = true;
+  }
+
+  private changePage(e: any) {
+    if (e.target.attributes.id.nodeValue === 'reports') {
+      this.editor = true;
+      this.home = false;
+    } else if (e.target.attributes.id.nodeValue === 'home') {
+      this.editor = false;
+      this.home = true;
+    }
+  }
 }
--- rs-app/src/app/app.module.ts
+++ rs-app/src/app/app.module.ts
@@ -2,13 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule}  from '@angular/platform-browser/animations';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-import { MdButtonModule, MdCheckboxModule, MdCardModule } from '@angular/material';
+import { MdButtonModule, MdCheckboxModule, MdCardModule, MatSidenavModule, MdIconModule } from '@angular/material';
+import { FroalaEditorModule, FroalaViewModule } from 'angular-froala-wysiwyg';
 
 import { AppComponent } from './app.component';
+import { ReportComponent } from './report/report.component';
+import { ReportEditorComponent } from './report-editor/report-editor.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReportComponent,
+    ReportEditorComponent
   ],
   imports: [
     BrowserModule,
@@ -16,7 +21,11 @@ import { AppComponent } from './app.component';
     NoopAnimationsModule,
     MdButtonModule,
     MdCheckboxModule,
-    MdCardModule
+    MdCardModule,
+    MatSidenavModule,
+    MdIconModule,
+    FroalaEditorModule.forRoot(), 
+    FroalaViewModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
