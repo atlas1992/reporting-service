dc626ad1c1ee870741da6f7562d1c1196c433a99
Shannon Liang <sliang@c.systemsinmotion.com>
2017-09-28 10:25:16 -0400

    Modify db connections

--- rs-api/dbconnection.js
+++ rs-api/dbconnection.js
@@ -1,7 +1,7 @@
 var mysql=require('mysql');
 var connection=mysql.createPool({
 
-host:'localhost',
+host:'/cloudsql/feisty-beacon-180520:us-east1:reportingservice',
 user:'root',
 password:'secret',
 database:'reportingservice'
