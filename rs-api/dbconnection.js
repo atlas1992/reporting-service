2183f45d205d3bdfb60da0dbb751fc07bccafb86
Shannon Liang <sliang@c.systemsinmotion.com>
2017-09-28 13:44:43 -0400

    db connections

--- rs-api/dbconnection.js
+++ rs-api/dbconnection.js
@@ -1,10 +1,14 @@
 var mysql=require('mysql');
 var connection=mysql.createPool({
 
+host: '104.196.209.109',
 user:'root',
 password:'secret',
-socketPath: '/cloudsql/feisty-beacon-180520:us-east1:reportingservice',
-database:'rsdb'
-
+database:'rsdb',
+stream: function(opts) {
+    var socket = net.connect(opts.config.port, opts.config.host);
+    socket.setKeepAlive(true);
+    return socket;
+  }
 });
 module.exports=connection;
