dd5eee1b45929560394dff530656031751152039
Shannon Liang <sliang@c.systemsinmotion.com>
2017-09-28 10:38:20 -0400

    Modify db connections, change db name

--- rs-api/dbconnection.js
+++ rs-api/dbconnection.js
@@ -4,7 +4,7 @@ var connection=mysql.createPool({
 host:'/cloudsql/feisty-beacon-180520:us-east1:reportingservice',
 user:'root',
 password:'secret',
-database:'reportingservice'
+database:'rsdb'
 
 });
 module.exports=connection;
