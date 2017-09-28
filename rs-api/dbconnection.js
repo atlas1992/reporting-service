aaf0b68b0c12cbb4abe275e1ebbf4c70ee3f9d8c
Shannon Liang <sliang@c.systemsinmotion.com>
2017-09-28 14:06:38 -0400

    db connections

--- rs-api/dbconnection.js
+++ rs-api/dbconnection.js
@@ -1,14 +1,9 @@
 var mysql=require('mysql');
 var connection=mysql.createPool({
 
-host: '104.196.209.109',
+host: 'localhost',
 user:'root',
 password:'secret',
-database:'rsdb',
-stream: function(opts) {
-    var socket = net.connect(opts.config.port, opts.config.host);
-    socket.setKeepAlive(true);
-    return socket;
-  }
+database:'rsdb'
 });
 module.exports=connection;
